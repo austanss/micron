a5f6c553e67e4d7d72b86b44e5403ca54c5cf8e4
rizet <ryzenupp@hotmail.com>
2020-10-17 18:51:44 -0400

    test: set interrupt flag

--- microCORE/HardwareAbstractionLayer/Headers/Terminal.h
+++ microCORE/HardwareAbstractionLayer/Headers/Terminal.h
@@ -13,19 +13,18 @@ class Terminal
         uint16_t* buffer;
 
 public:
-        static constexpr const char* Status = "$WHITE![$LIGHT_BLUE!-$WHITE!] $LIGHT_GREY!\0";
-        static constexpr const char* Good = "$WHITE![$LIGHT_GREEN!+$WHITE!] $LIGHT_GREY!\0";
-        static constexpr const char* Fail = "$WHITE![$LIGHT_GREEN!X$WHITE!] $LIGHT_GREY!\0";
-        static constexpr const char* EOL = "\n";
-        static Terminal &instance();
-
-        void put_entry_at(char c, uint8_t color, size_t x, size_t y);
-        void put_char(char c, uint8_t color);
-        void write(const char* data, size_t size);
-        void write(const char* data);
-        void write(int num);
-        void println(const char* data = "");
-        void shift();
+    static constexpr const char* Status = "$WHITE![$LIGHT_BLUE!-$WHITE!] $LIGHT_GREY!\0";
+    static constexpr const char* Good = "$WHITE![$LIGHT_GREEN!+$WHITE!] $LIGHT_GREY!\0";
+    static constexpr const char* Fail = "$WHITE![$RED!X$WHITE!] $LIGHT_GREY\0";
+    static constexpr const char* EOL = "\n";
+    static Terminal &instance();
+    void put_entry_at(char c, uint8_t color, size_t x, size_t y);
+    void put_char(char c, uint8_t color);
+    void write(const char* data, size_t size);
+    void write(const char* data);
+    void write(int num);
+    void println(const char* data = "");
+    void shift();
 	void clear();
 	void setCursor(int x, int y);
 
